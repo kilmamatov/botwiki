5fb06dd2e4162be1ebc45519737d54700aaa58c4
kilmamatov <117253418+kilmamatov@users.noreply.github.com>
2023-01-26 03:11:09 +0500

    #1

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ wikipedia.set_lang('ru')
 
 @bot.message_handler(commands=["Random"])
 def random(m):
-    webbrowser.open('https://ru.wikipedia.org/wiki/%D0%A1%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F:%D0%A1%D0%BB%D1%83%D1%87%D0%B0%D0%B9%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0', new=0, autoraise=True)
+    bot.send_message(m.chat.id, 'https://ru.wikipedia.org/wiki/%D0%A1%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F:%D0%A1%D0%BB%D1%83%D1%87%D0%B0%D0%B9%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0')
 
 
 # Функция, обрабатывающая команду /start
