4a05423d416ae8d82c8df0f3a38e7157f3e80ad2
kilmamatov <117253418+kilmamatov@users.noreply.github.com>
2023-01-28 17:06:25 +0500

    #1

--- main.py
+++ main.py
@@ -13,11 +13,13 @@ def welcom(message):
     markup = telebot.types.ReplyKeyboardMarkup()
     start_btn = telebot.types.KeyboardButton("/start")
     wiki_btn = telebot.types.KeyboardButton("/wiki")
-    wikieng_btn = telebot.types.KeyboardButton("/wikieng")
     random_btn = telebot.types.KeyboardButton("/Random")
     meme_bth = telebot.types.KeyboardButton("/meme")
-    markup.add(start_btn, wiki_btn, random_btn, wikieng_btn, meme_bth)
+    markup.add(start_btn, wiki_btn, random_btn, meme_bth)
     bot.send_message(message.chat.id, 'Привет! Это бот wikipedia', reply_markup=markup)
+
+
+wikipedia.set_lang('ru')
 
 
 @bot.message_handler(commands=["Random"])
@@ -28,7 +30,6 @@ def random(m):
 # Функция, обрабатывающая команду /start
 @bot.message_handler(commands=["wiki"])
 def wiki(m, res=False):
-    wikipedia.set_lang('ru')
     bot.send_message(m.chat.id, 'Отправьте мне любое слово, и я найду его значение')
 
     def getwiki(s):
@@ -67,53 +68,12 @@ def wiki(m, res=False):
         bot.send_message(message.chat.id, getwiki(message.text), reply_markup=markup)
 
 
-@bot.message_handler(commands=["wikieng"])
-def wikieng(m, res=False):
-    wikipedia.set_lang('en')
-    bot.send_message(m.chat.id, 'Отправьте мне любое слово, и я найду его значение на Английском')
-
-    def getwiki(s):
-        try:
-            ny = wikipedia.page(s)
-            # Получаем первую тысячу символов
-            wikitext = ny.content[:1000]
-            # Разделяем по точкам
-            wikimas = wikitext.split('.')
-            # Отбрасываем всЕ после последней точки
-            wikimas = wikimas[:-1]
-            # Создаем пустую переменную для текста
-            wikitext2 = ''
-            # Проходимся по строкам, где нет знаков «равно» (то есть все, кроме заголовков)
-            for x in wikimas:
-                if not ('==' in x):
-                    # Если в строке осталось больше трех символов, добавляем ее к нашей переменной и возвращаем утерянные при разделении строк точки на место
-                    if (len((x.strip())) > 3):
-                        wikitext2 = wikitext2 + x + '.'
-                else:
-                    break
-            # Теперь при помощи регулярных выражений убираем разметку
-            wikitext2 = re.sub('\{[^\{\}]*\}', '', wikitext2)
-            # Возвращаем текстовую строку
-            return wikitext2
-        # Обрабатываем исключение, которое мог вернуть модуль wikipedia при запросе
-        except Exception as e:
-            return 'В энциклопедии мало информации, подробнее по ссылке'
-
-    # Получение сообщений от юзера
-    @bot.message_handler(content_types=["text"])
-    def handle_text2(message):
-        markup = telebot.types.InlineKeyboardMarkup()
-        btn_my_site = telebot.types.InlineKeyboardButton(text='Full', url='https://en.wikipedia.org/w/index.php?search=&title=' + message.text)
-        markup.add(btn_my_site)
-        bot.send_message(message.chat.id, getwiki(message.text), reply_markup=markup)
-
-
 @bot.message_handler(commands=["meme"])
 def memet(m, res=False):
     markup = telebot.types.InlineKeyboardMarkup()
     btn_my_site = telebot.types.InlineKeyboardButton(text='видео', url='https://www.youtube.com/watch?v=dQw4w9WgXcQ')
     markup.add(btn_my_site)
-    bot.send_message(m.chat.id, 'Эта кнопка пока не работает, лучше посмотрите видео', reply_markup=markup)
+    bot.send_message(m.chat.id, 'Эта кнопка для поднятия настроения', reply_markup=markup)
 
 
 bot.polling(none_stop=True, interval=0)
